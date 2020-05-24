f32cd6f7b51a794c2ccdc4278d216af9ac8372f9
dark-shade <sr282828@gmail.com>
2020-05-24 00:59:46 -0500

    added leetcode crawler

---
+++ crawler/crawlers/crawlers/spiders/leetcode_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class LeetCodeSpider(scrapy.Spider):
+    name = "leetcode"
+
+    def start_requests(self):
+        urls = [
+            'https://leetcode.com/dark_shade/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        solved_data = response.css('span.badge.progress-bar-success::text')[3].get()
+        sols = solved_data.split()
+        yield {
+            'solved': sols[0],
+            'total': sols[2],
+        }
