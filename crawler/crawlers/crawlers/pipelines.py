c544ce77e56b34925a35271fd6a83cba59994a1e
dark-shade <sr282828@gmail.com>
2020-05-24 01:30:46 -0500

    added sqlite to the crawler pipeline

--- crawler/crawlers/crawlers/pipelines.py
+++ crawler/crawlers/crawlers/pipelines.py
@@ -5,7 +5,29 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
+from sqlite3 import Error
+import logging
 
-class CrawlersPipeline:
+class SqlitePipeline:
+    def __init__(self, sqlite_file):
+        try:
+            self.connection = sqlite3.connect(sqlite_file)
+            self.cursor = self.connection.cursor()
+        except Error as e:
+            logging.error(e)
+        finally:
+            if self.connection:
+                self.connection.close()
+
+    def open_spider(self, spider):
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS leetcodedata ' \
+                    '(id INTEGER PRIMARY KEY, solved INTEGER, total INTEGER, date TEXT)')
+
+    def close_spider(self, spider):
+        self.connection.commit()
+        self.connection.close()
+
     def process_item(self, item, spider):
+        self.cursor.execute('INSERT INTO leetcodedata (solved, total, date) VALUES (?, ?, ?)', item["solved"], item["total"], item["date"])
         return item
